93546fed42f05e9ff69ee10e976b28128dd05c89
JorikVanRuiswijk <jorik.van.ruiswijk@mediamonks.com>
2022-02-28 11:38:45 +0100

    Add async API call and render video conditionally

--- giffy/src/App.js
+++ giffy/src/App.js
@@ -8,8 +8,23 @@ class App extends Component {
     this.state = {
       searchTerm: "",
       hintText: "",
+      gif: "",
     };
   }
+
+  searchGiphy = async (searchTerm) => {
+    try {
+      const response = await fetch(
+        `https://api.giphy.com/v1/gifs/search?api_key=rGcYJZqhoYZBJXKR0TKyTDuyBku64SLR&q=${searchTerm}&limit=25&offset=0&rating=g&lang=en`
+      );
+      const { data } = await response.json();
+
+      this.setState((prevState, props) => ({
+        ...prevState,
+        gif: data[0],
+      }));
+    } catch (error) {}
+  };
 
   handleChange = (event) => {
     const { value } = event.target;
@@ -18,23 +33,29 @@ class App extends Component {
       searchTerm: value,
       hintText: value.length > 2 ? `Hit enter to search ${value}` : "",
     }));
-    if (value.length > 2) {
-    }
   };
   handleKeyPress = (event) => {
     const { value } = event.target;
     if (value.length > 2 && event.key === "Enter") {
-      alert(`Search for ${value}`);
+      this.searchGiphy(value);
     }
   };
 
   render() {
-    const { searchTerm } = this.state;
+    const { searchTerm, gif } = this.state;
 
     return (
       <div className="page">
         <Header />
         <div className="search grid">
+          {gif && (
+            <video
+              className="grid-item video"
+              autoPlay
+              loop
+              src={gif.images.original.mp4}
+            />
+          )}
           <input
             className="input grid-item"
             placeholder="Type something.."
